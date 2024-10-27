5387eda19328d40058337b4561d1ed3f5b744308
hbmudav <udav-hbm@mail.ru>
2024-10-27 20:13:27 +0800

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def fake_divide (first, second):
+    if second == 0:
+        return 'Ошибка'
+    else:
+        return first / second
+# print(fake_divide(1,2))
---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import fake_divide
+from true_math import true_divide
+
+
+print(fake_divide(69, 3))
+print(fake_divide(3, 0))
+print(true_divide(49, 7))
+print(true_divide(15, 0))
